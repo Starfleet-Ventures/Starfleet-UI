0dd52843ab2652b2c318c022097a6d143514b26e
Ishan Sharma <ishan@yactraq.net>
2022-03-08 11:37:32 +0000

    reset placeholders

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,30 @@ const isAuth = (req,res,next) =>{
 
 
 app.use(express.static(filePath));
+app.get('/reset',isAuth,(req,res)=>{
+  const user = req.session.userId;
+  const userPath = path.join(__dirname,'map','output',user);
+  let resFlag = false;
+  try{
+    if(fs.existsSync(userPath)){
+      try{
+        fs.rmSync(userPath, { recursive: true, force: true });
+        resFlag = true;
+      }
+      catch(e){
+        console.error(e);
+        console.log("could not delete");
+        resFlag = false;
+      }
+    }
+  }
+  catch(e){
+    console.error(e);
+    console.log("outer catch");
+    resFlag = false;
+  }
+  resFlag === true ? res.redirect('/map-ui') : res.status(500).json({message: 'Something went wrong'});
+})
 app.get('/map-ui',isAuth,(req,res)=>{
   res.sendFile(path.join(__dirname,'..','dist','starfleet','index.html'));
 })

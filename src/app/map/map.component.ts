157fd14bef7a84a5370e90c3b6daef5dfcc62b79
Ishan Sharma <ishan@yactraq.net>
2022-04-12 06:53:28 +0000

    Bug Resolved: Map on Scrolling is not circular

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -28,7 +28,8 @@ export class MapComponent implements AfterViewInit {
   private initMap(): void {
     this.map = L.map('map', {
       center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      zoom: 3,
+      worldCopyJump: true
     });
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
